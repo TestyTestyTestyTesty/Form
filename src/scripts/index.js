3c752db869525fd3842f6ddbe3c8b3bf146bb261
STProjectPeople <tomasz.sikora@projectpeople.pl>
2022-01-27 23:11:56 +0100

    basic js validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,47 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('dom loaded');
+window.addEventListener("DOMContentLoaded", (event) => {
+  formSubmit();
+  function formSubmit() {
+    const form = document.querySelector(".form");
+    if (form) {
+      const peselRegex = new RegExp("^d{11}$");
+      let errors = form.querySelector(".form__errors");
+      form.addEventListener("submit", function (e) {
+        let firstName = form.querySelector("#first-name").value;
+        let lastName = form.querySelector("#last-name").value.trim();
+        let pesel = form.querySelector("#pesel").value.trim();
+        let phone = form.querySelector("#phone").value.trim();
+        let password = form.querySelector("#password").value.trim();
+        let messages = [];
+        errors.innerHTML = "";
+        e.preventDefault();
+
+        if (firstName === "" || firstName === null) {
+          messages.push("First name is required");
+        }
+        if (lastName === "" || lastName === null) {
+          messages.push("Last name is required");
+        }
+        if (pesel === "" || pesel === null) {
+          messages.push("PESEL is required");
+        } else if (pesel.length !== 11) {
+          messages.push("invalid PESEL length");
+        }
+        if (phone === "" || phone === null) {
+          messages.push("Phone is required");
+        }
+        if (password === "" || password === null) {
+          messages.push("Password is required");
+        } else if (password.length < 6) {
+          messages.push("Password is too short");
+        } else if (password.length > 30) {
+          messages.push("Password is too long");
+        }
+        messages.forEach((message) => {
+          const paragraph = document.createElement("p");
+          paragraph.innerHTML = message;
+          errors.appendChild(paragraph);
+        });
+      });
+    }
+  }
 });
